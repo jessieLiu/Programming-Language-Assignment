0ad7c0f174f58ee56079d03546ace0e1b3f5ac04
sqybi <sqybi@126.com>
2011-11-29 21:21:18 +0800

    solution of assignment 4 problem 2 added

---
+++ 04/02/postfix.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string s;
+
+string ParsePlusMinus(string);
+
+string ParseMultiplyDivide(string s)
+{
+    int n = s.size();
+
+    int k;
+    for (k = 0; k != n; ++k)
+        if (s[k] == '*' || s[k] == '/')
+            break;
+
+    if (k == n)
+        return ParsePlusMinus(s);
+    else
+        return ParseMultiplyDivide(s.substr(0, k)) + ParseMultiplyDivide(s.substr(k + 1, n - k - 1)) + s[k];
+}
+
+string ParsePlusMinus(string s)
+{
+    int n = s.size();
+
+    int k;
+    for (k = 0; k != n; ++k)
+        if (s[k] == '+' || s[k] == '-')
+            break;
+
+    if (k == n)
+        return s;
+    else
+        return ParsePlusMinus(s.substr(0, k)) + ParseMultiplyDivide(s.substr(k + 1, n - k - 1)) + s[k];
+}
+
+int main()
+{
+    cin >> s;
+    cout << ParseMultiplyDivide(s) << endl;
+
+    return 0;
+}
+
